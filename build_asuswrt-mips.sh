d216045d7a24e6f71cedd468c265b76662be3685
st-ty1 <stephan.tratzky@gmail.com>
2021-01-14 20:06:07 +0100

    Add files via upload

--- build_asuswrt-mips.sh
+++ build_asuswrt-mips.sh
@@ -5,16 +5,18 @@ ASUSWRT_PATCHES_DIR=$HOME/asuswrt_artix  ## path to your local copy of this repo
 
 PATH="$ASUSWRT_REPO_DIR/tools/brcm/hndtools-mipsel-uclibc/bin:$PATH"
 
-cd $ASUSWRT_REPO_DIR && git clean -dxf && git reset --hard
-
+cd $ASUSWRT_REPO_DIR 
+git clean -dxf && git reset --hard
 #git pull
 git checkout 374.43_2-update
 
+clear
+
+## incomplete data in sources of asuswrt_johns fork
 cp $ASUSWRT_PATCHES_DIR/pptpd-1.3.4/plugins/Makefile $ASUSWRT_REPO_DIR/release/src/router/pptpd/plugins/Makefile
-
 cp $ASUSWRT_PATCHES_DIR/openssl-1.0.2u/Makefile.org $ASUSWRT_REPO_DIR/release/src/router/openssl-1.0
-
 cp -r $ASUSWRT_PATCHES_DIR/wget-1.19.5/* $ASUSWRT_REPO_DIR/release/src/router/wget
+cp -v $ASUSWRT_PATCHES_DIR/cipher-negotiation.rst $ASUSWRT_REPO_DIR/release/src/router/openvpn/doc/man-sections/
 
 ## patch because of newer awk in Artix; Makefile.in need to be patched not Makefile.am (not used)!
 patch -p1 -d$ASUSWRT_REPO_DIR/release/src/router/libgpg-error-1.10 < $ASUSWRT_PATCHES_DIR/libgpg-error.patch 
@@ -23,19 +25,21 @@ patch -p1 -d$ASUSWRT_REPO_DIR/release/src/router/libgpg-error-1.10 < $ASUSWRT_PA
 rm -f $ASUSWRT_REPO_DIR/release/src/router/wget/Makefile.in
 
 patch -i $ASUSWRT_PATCHES_DIR/Makefile.patch $ASUSWRT_REPO_DIR/release/src/router/Makefile
-
-cp -v $ASUSWRT_PATCHES_DIR/cipher-negotiation.rst $ASUSWRT_REPO_DIR/release/src/router/openvpn/doc/man-sections/
+#cp -v $ASUSWRT_PATCHES_DIR/Makefile $ASUSWRT_REPO_DIR/release/src/router/Makefile
 
 rm -f $ASUSWRT_REPO_DIR/release/src/router/nettle/desdata.stamp
+rm -f $ASUSWRT_REPO_DIR/release/src/router/curl/Makefile
 
+## due to use of gcc-10
 patch -i $ASUSWRT_PATCHES_DIR/mksquashfs.c.patch $ASUSWRT_REPO_DIR/release/src-rt/linux/linux-2.6/scripts/squashfs/mksquashfs.c
 patch -p1 -d$ASUSWRT_REPO_DIR/release/src/router/config < $ASUSWRT_PATCHES_DIR/config_gcc10.patch
 
-rm -f $ASUSWRT_REPO_DIR/release/src/router/curl/Makefile
+patch -i $ASUSWRT_PATCHES_DIR/std-gnu11.m4.patch $ASUSWRT_REPO_DIR/release/src/router/nano/m4/std-gnu11.m4
 
+## use of too old autoconf macro, which crashes
 patch -i $ASUSWRT_PATCHES_DIR/libxml2_configure.in.patch $ASUSWRT_REPO_DIR/release/src/router/libxml2/configure.in
 
-cd release/src-rt && make rt-n16     ## for rt-n16  do not start with rt-n66u from src-rt!
-#cd release/src-rt-6.x && make rt-ac66u       ## for rt-ac66u 
+#cd release/src-rt && make rt-n16     ## for rt-n16  do not start with rt-n66u from src-rt!
+cd release/src-rt-6.x && make rt-ac66u       ## for rt-ac66u 
 #cd release/src-rt-6.x && make rt-n66u       ## for rt-n66u 
 
